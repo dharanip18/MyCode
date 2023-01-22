3ec9fbfdbe70f20c2e4c90a58d983a8b90b92bc3
dharani-p18 <dharanipappannan18@gmail.com>
2023-01-22 20:21:47 +0530

    Chapter 03 - Laying the Foundation

--- app.js
+++ app.js
@@ -1,48 +1,77 @@
 /***
-*
-* Created A Server
-* HMR - Hot Module Replacement - saving is updated in browser
-* File Watcher algorithm - C++
-* BUNDLING
-* MINIFY
-* Cleaning our Code
-* Dev abd Production Build
-* Super Fast build algorithm
-* Image Optimization
-* Caching while development - .parcel-cache
-* Compression
-* Compatble with older version of browser
-* HTTPS on dev  npx parcel index.html --https - https://localhost:1234
-* port Number - If you open other project port must vary - parcel takes care
-* Consistent Hashing Algorithm - to do all the bundling
-* Zero Config
-*
-*
-*
-* Transitive Dependencies
-*/
+ *
+ * Created A Server
+ * HMR - Hot Module Replacement - saving is updated in browser
+ * File Watcher algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning our Code
+ * Dev abd Production Build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching while development - .parcel-cache
+ * Compression
+ * Compatble with older version of browser
+ * HTTPS on dev  npx parcel index.html --https - https://localhost:1234
+ * port Number - If you open other project port must vary - parcel takes care
+ * Consistent Hashing Algorithm - to do all the bundling
+ * Zero Config
+ * Tree Shaking - Removing unwanted code -importing a library gives access to 10 or 20 helper function but we only need 2,3 fns. parcel will ignore all unused
+ *
+ *
+ * Transitive Dependencies
+ */
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const headingR = React.createElement(
-    "h1",
-    {},
-    "Hello! This is created using React! I'm from Parcel"
-    
-  );
-  const heading = React.createElement(
-    "h2",
-    { id: "tittle", className: "head" },
-    "Hello! This is created using React!"
-  );
+// const headingR = React.createElement(
+//   "h1",
+//   { key: "h1" },
+//   "Hello! This is created using React! I'm from Parcel"
+// );
+// const heading = React.createElement(
+//   "h2",
+//   {
+//     id: "tittle",
+//     key: "h2",
+//     className: "head",
+//   },
+//   "Hello! This is created using React!"
+// );
 
-  const container = React.createElement("div", { id: "container" }, [
-    headingR,
-    heading,
-  ]);
-  console.log(heading);
-  const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(heading);
 
-  // passing react element inside the root
-  root.render(container);
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "container",
+//   },
+//   [headingR, heading]
+// );
+
+//React Element
+const heading = (
+  <h1 id="title" key="h1">
+    This is React Element
+  </h1>
+);
+
+// Functional Component
+const HeaderComponent = () => {
+  return (
+    <div>
+      {heading}
+      <h1>This is functional component</h1>
+      <h2>This is h2 tag</h2>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// passing react element inside the root
+// root.render(heading);
+
+//render Component
+root.render(<HeaderComponent />);
